d1ab76d5e7942036e1d3d2553f0359f43f70850e
ciprutdavid <davidciprut@mail.tau.ac.il>
2021-08-21 20:06:41 +0300

    cmmt

--- project/pretraining/tokenization.py
+++ project/pretraining/tokenization.py
@@ -5,25 +5,4 @@ from tokenizers.models import WordPiece
 from tokenizers.pre_tokenizers import Whitespace
 
 
-SERVER_PATH = "/home/yandex/AMNLP2021/data/wiki/all"
-
-class Tokenizer:
-
-    def __init__(self,tokenizer,vocab=None):
-
-        self.wordpiece_tokenizer = 0
-        self.sentencepiece_tokenizer = 0
-        self.vocab = 0
-
-    def build_vocab(self):
-        print()
-
-
-
-
-
-if __name__ == "__main__":
-    sentence = "this is some sentence [UNK]"
-
-    pre_tok = Whitespace()
-    print(pre_tok.pre_tokenize_str(sentence))
+SERVER_DATA_PATH = "/home/yandex/AMNLP2021/data/wiki/all"
