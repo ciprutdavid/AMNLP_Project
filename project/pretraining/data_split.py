99018d9687cc9b446f33a2fb21a9b5f486ea30e4
David-bz <davidbzach@gmail.com>
2021-08-27 12:51:45 +0300

    Small additions to data_split

--- project/pretraining/data_split.py
+++ project/pretraining/data_split.py
@@ -2,15 +2,19 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.util import ngrams
+from nltk.tokenize import WordPunctTokenizer
 import numpy as np
 import time
+import os
 
-DATA_PATH = "./data/wiki/all"
-PROCESSED_DATA_PATH = "./data/wiki/processed"
+DATA_PATH = "E:/Studies/TAU/NLP/all"
+PROCESSED_DATA_PATH = "E:/Studies/TAU/NLP/processed"
 STOPWORDS_LIST = stopwords.words('english')
+nltk.download('punkt')
+
 VAL_SET_SIZE = 500
-TRAIN_DATA_PATH = "./data/wiki/train"
-VAL_DATA_PATH  = "./data/wiki/test"
+TRAIN_DATA_PATH = "E:/Studies/TAU/NLP/train"
+VAL_DATA_PATH  = "E:/Studies/TAU/NLP/test"
 
 
 def ends_with_punctuation(string):
@@ -23,7 +27,9 @@ def unwanted_line(string):
 
 
 def preprocess_wiki():
-    data = open(DATA_PATH, 'r')
+    if os.stat(PROCESSED_DATA_PATH).st_size > 0:
+        return sum(1 for _ in open(PROCESSED_DATA_PATH, 'r' ,errors="ignore"))
+    data = open(DATA_PATH, 'r' ,errors="ignore")
     processed = open(PROCESSED_DATA_PATH, 'a')
     num_lines =0
     for paragraph in data:
@@ -31,6 +37,14 @@ def preprocess_wiki():
             num_lines += 1
             processed.write(paragraph)
     return num_lines
+
+
+def data_reader(ob):
+    for line in ob:
+        if line:
+            yield line
+        else:
+            raise EOFError
 
 def split_train_validation():
     num_paragraphs = preprocess_wiki()
@@ -44,6 +58,30 @@ def split_train_validation():
             val.write(line)
         else:
             train.write(line)
+
+def get_word_histogram(line, tokenizer):
+    raise NotImplementedError
+
+def find_maximal_ngrams(line, hist, tokenizer):
+    raise NotImplementedError
+
+def select_masked_tokens(line, ngrams_list):
+    # TODO: add [QUESTION] token for n-1 of the occurrences for 1 (or more) repeated words from ngram_list
+    raise NotImplementedError
+
+def add_question_tokens():
+    data = open(PROCESSED_DATA_PATH, 'r') # Consider to split train and validation 'lazy'ly after this function.
+    word_tokenizer = WordPunctTokenizer()
+    reader = data_reader(data)
+    while True:
+        try:
+            line = next(reader)
+            hist = get_word_histogram(data, word_tokenizer)
+            ngrams_list = find_maximal_ngrams(data, hist, word_tokenizer)
+            processed_line = select_masked_tokens(line, ngrams_list)
+            # TODO: Maybe here it's a good place to add (stochaticly) to train/validation
+        except EOFError:
+            break
 
 
 def has_recurring_span(paragraph):
