12014bed3f15378ddafdb7fead7accf4924a5a33
ciprutdavid <davidciprut@mail.tau.ac.il>
2021-09-02 10:42:37 +0300

    cmmt

--- project/pretraining/t5_baseline_train_config.py
+++ project/pretraining/t5_baseline_train_config.py
@@ -1,31 +1,34 @@
-import transformers
-from transformers import Trainer, T5Model, T5Config, AutoTokenizer
+import torch
+from transformers import Trainer, T5Model, T5Config, AutoTokenizer,TrainingArguments
 import t5_baseline_dataset as baseline_data
 
 
 
 TRAIN_PATH = "/content/drive/MyDrive/Colab Notebooks/AMNLP_project/data/train"
 VAL_PATH = "/content/drive/MyDrive/Colab Notebooks/AMNLP_project/data/val"
+train_data = baseline_data.load_data(TRAIN_PATH)
+val_data = baseline_data.load_data(VAL_PATH)
+torch.cuda.set_device(0)
 model_config = T5Config()
+model = T5Model(model_config).to('cuda')
 tokenizer = AutoTokenizer.from_pretrained('t5-base', cache_dir="t5_baseline_pretrain_output_dir/tokenizer_cache/")
 args = {
     'output_dir':"t5_baseline_pretrain_output_dir/",
     'do_eval':True,
     'evaluation_strategy':"steps",
     'num_train_epochs':20,
-    # 'lr_scheduler_type':[],
-    'save_strategy':[],
     'save_steps':1000,
     'save_total_limit':10,
     'eval_steps':1000,
+    'dataloader_pin_memory':False,
+    'per_device_train_batch_size':8
 }
 
 trainer_config ={
-    'model': T5Model(model_config),
-    'args':args,
-    'data_collator':baseline_data.T5_Collate(tokenizer),
-    'train_dataset':baseline_data.load_data(TRAIN_PATH),
-    'eval_dataset':baseline_data.load_data(VAL_PATH),
+    'model': model,
+    'args':TrainingArguments(**args),
+    'data_collator':baseline_data.T5_Collate(tokenizer,'cuda'),
+    'train_dataset':train_data,
+    'eval_dataset':val_data,
     'tokenizer':tokenizer,
-    # 'compute_metrics':[],
 }
