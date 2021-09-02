1efd6d1fe5475a4e9f085e413c608ba2ccd619fe
ciprutdavid <davidciprut@mail.tau.ac.il>
2021-09-02 16:57:42 +0300

    cmmt

--- project/pretraining/t5_baseline_train_config.py
+++ project/pretraining/t5_baseline_train_config.py
@@ -5,6 +5,7 @@ import t5_baseline_dataset as baseline_data
 TRAIN_PATH = "/home/yandex/AMNLP2021/davidciprut/AMNLP_Project/project/data/train"
 VAL_PATH = "/home/yandex/AMNLP2021/davidciprut/AMNLP_Project/project/data/test"
 
+torch.cuda.device(0)
 train_data = baseline_data.load_data(TRAIN_PATH)
 val_data = baseline_data.load_data(VAL_PATH)
 tokenizer = AutoTokenizer.from_pretrained('t5-base', cache_dir="t5_baseline_pretrain_output_dir/tokenizer_cache/")
@@ -20,9 +21,10 @@ args = {
     'save_total_limit':10,
     'eval_steps':1000,
     'dataloader_pin_memory':False,
-    'per_device_train_batch_size':4,
-    'gradient_accumulation_steps' : 64,
-    'warmup_ratio': 0.1
+    'per_device_train_batch_size':8,
+    'gradient_accumulation_steps' : 32,
+    'warmup_ratio': 0.1,
+    'logging_steps': 1
 }
 
 trainer_config ={
