92a7fab660a20d47b7c0f08ab7750144685a8697
David-bz <davidbzach@gmail.com>
2021-09-12 02:47:05 +0300

    collating

--- project/pretraining/splinter_t5_model.py
+++ project/pretraining/splinter_t5_model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from transformers import T5EncoderModel, T5Config
-
+import numpy as np
 MASK_ID = 32099  # mask id of <extra_id_0>
 DIM = 512  # seq_len
 
@@ -17,7 +17,7 @@ class SplinterT5Model(torch.nn.Module):
     def forward(self, input_ids, attention_mask):
         question_indices = (input_ids == MASK_ID).long().view(-1)  # dim = NUM_OF_BATCH*SEQ_LEN
         relevant_attention_mask = attention_mask[question_indices, :]  # dim = BATCH_SIZE x SEQ_LEN
-        relevant_attention_mask[relevant_attention_mask == 0] = float('-inf')  # dim BATCH_SIZE x SEQ_LEN
+        relevant_attention_mask[relevant_attention_mask == 0] = np.NINF  # dim BATCH_SIZE x SEQ_LEN
 
         X_T = self.t5_encoder(input_ids)  # dim = NUM_OF_BATCH. x BATCH_SIZE x SEQ_LEN
         X_T = X_T * relevant_attention_mask.unsqeeeze(-1)
