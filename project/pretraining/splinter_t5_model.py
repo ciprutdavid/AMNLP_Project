719683a269c83258f36a4a0349dbb696da1ae4e2
ciprutdavid <davidciprut@mail.tau.ac.il>
2021-09-12 12:47:51 +0300

    cmmt

--- project/pretraining/splinter_t5_model.py
+++ project/pretraining/splinter_t5_model.py
@@ -11,16 +11,16 @@ class SplinterT5Model(torch.nn.Module):
     def __init__(self):
         super(SplinterT5Model, self).__init__()
         self.t5_encoder = T5EncoderModel(T5Config())
-        self.S = nn.Parameter(torch.randn(size=(DIM, DIM)), requires_grad=True)
-        self.E = nn.Parameter(torch.randn(size=(DIM, DIM)), requires_grad=True)
+        self.S = nn.Parameter(torch.randn(size=(DIM, DIM)),requires_grad=True)
+        self.E = nn.Parameter(torch.randn(size=(DIM, DIM)),requires_grad=True)
 
     def forward(self, input_ids, attention_mask):
         question_indices = (input_ids == MASK_ID).view(-1) # dim = NUM_OF_BATCH*SEQ_LEN
-        # relevant_attention_mask = attention_mask  # dim = BATCH_SIZE x SEQ_LEN
-        # relevant_attention_mask[relevant_attention_mask == 0] = float('-inf')  # dim BATCH_SIZE x SEQ_LEN
+        relevant_attention_mask = attention_mask  # dim = BATCH_SIZE x SEQ_LEN
+        relevant_attention_mask[relevant_attention_mask == 0] = float('-inf')  # dim BATCH_SIZE x SEQ_LEN
 
         X_T = self.t5_encoder(input_ids).last_hidden_state  # dim = NUM_OF_BATCH. x BATCH_SIZE x SEQ_LEN
-        # X_T = X_T * torch.unsqueeze(relevant_attention_mask,-1)
+        X_T = X_T * torch.unsqueeze(relevant_attention_mask,-1)
         X = torch.transpose(X_T, 2, 1)  # dim = NUM_OF_BATCH x SEQ_LEN x BATCH_SIZE
 
         start_prob = F.softmax(torch.nan_to_num((X_T @ self.S @ X).view(-1, DIM)[question_indices, :],nan=float('-inf')),-1)
