9aea3c47b9afd2c70dc2a6ddc27a251a53d1c175
ciprutdavid <davidciprut@mail.tau.ac.il>
2021-09-12 12:26:37 +0300

    cmmt

--- project/pretraining/splinter_t5_model.py
+++ project/pretraining/splinter_t5_model.py
@@ -11,25 +11,19 @@ class SplinterT5Model(torch.nn.Module):
     def __init__(self):
         super(SplinterT5Model, self).__init__()
         self.t5_encoder = T5EncoderModel(T5Config())
-        self.S = nn.Parameter(torch.randn(size=(DIM, DIM)), requires_grad=True)
-        self.E = nn.Parameter(torch.randn(size=(DIM, DIM)), requires_grad=True)
+        self.S = nn.Parameter(torch.randn(size=(DIM, DIM)),requires_grad=True)
+        self.E = nn.Parameter(torch.randn(size=(DIM, DIM)),requires_grad=True)
 
     def forward(self, input_ids, attention_mask):
-        print((input_ids == MASK_ID).long().shape)
-        question_indices = (input_ids == MASK_ID).long().view(-1)  # dim = NUM_OF_BATCH*SEQ_LEN
-        print(question_indices.shape)
-        relevant_attention_mask = attention_mask[question_indices, :]  # dim = BATCH_SIZE x SEQ_LEN
-        print(relevant_attention_mask.shape)
-        relevant_attention_mask[relevant_attention_mask == 0] = -1 # dim BATCH_SIZE x SEQ_LEN
-        print(relevant_attention_mask.shape)
-        X_T = self.t5_encoder(input_ids).last_hidden_state   # dim = NUM_OF_BATCH. x BATCH_SIZE x SEQ_LEN
-        print(X_T.shape)
-        print(relevant_attention_mask.shape)
-        print(relevant_attention_mask.unsqueeze(-1).shape)
-        X_T = X_T * relevant_attention_mask.unsqueeze(-1)
+        question_indices = (input_ids == MASK_ID).view(-1) # dim = NUM_OF_BATCH*SEQ_LEN
+        relevant_attention_mask = attention_mask  # dim = BATCH_SIZE x SEQ_LEN
+        relevant_attention_mask[relevant_attention_mask == 0] = float('-inf')  # dim BATCH_SIZE x SEQ_LEN
+
+        X_T = self.t5_encoder(input_ids).last_hidden_state  # dim = NUM_OF_BATCH. x BATCH_SIZE x SEQ_LEN
+        X_T = X_T * torch.unsqueeze(relevant_attention_mask,-1)
         X = torch.transpose(X_T, 2, 1)  # dim = NUM_OF_BATCH x SEQ_LEN x BATCH_SIZE
 
-        start_scores = (X_T @ self.S @ X).view(-1, DIM)[question_indices, :]
-        end_scores = (X_T @ self.S @ X).view(-1, DIM)[question_indices, :]
+        start_scores = torch.nan_to_num((X_T @ self.S @ X).view(-1, DIM)[question_indices, :],nan=float('-inf'))
+        end_scores = torch.nan_to_num((X_T @ self.S @ X).view(-1, DIM)[question_indices, :],nan=float('-inf'))
 
         return start_scores, end_scores
