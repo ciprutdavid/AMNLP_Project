0226710e0834256aac637a284d39c4b0647aff80
David-bz <davidbzach@gmail.com>
2021-09-17 23:34:58 +0300

    tests

--- project/finetuning/test_models.py
+++ project/finetuning/test_models.py
@@ -33,12 +33,10 @@ class EvaluateModel:
             if 'context' not in line:
                 print("Metadata entry of the dataset")
                 return
-            y = self._find_start_end_indices(self.tokenizer(line['context']).input_ids,
-                                            self.tokenizer(line['qas'][0]['answers'][0]).input_ids)
+            y = self._find_start_end_indices(self.tokenizer(line['context'].lower()).input_ids,
+                                            self.tokenizer(line['qas'][0]['answers'][0].lower()).input_ids)
             num_occ_of_answer = len(line['qas'][0]['detected_answers'][0]['char_spans'])
-            if len(y) == num_occ_of_answer:
-                print(1)
-            else:
+            if len(y) != num_occ_of_answer:
                 print(0)
             prepared_line = line['context'] +  " </s> " + line['qas'][0]['question'] + " " + "<extra_id_0>"
             tokenized = self.tokenizer(prepared_line, padding = 'max_length', truncation = True, max_length = DIM).input_ids
